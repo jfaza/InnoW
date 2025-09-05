d5b76edf9a14affa9e0ba40db44df9ffdaa99148
Javid Hussain Fazaeli <luckyjavid@gmail.com>
2025-09-05 00:44:10 -0500

    WIP: React app (CSS Modules) in in innoweg-ui subfolder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+};
